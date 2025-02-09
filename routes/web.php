678cc955d473b774386943de367a509fcfca8c54
Carmel Houssou <carme.lhous16@gmail.com>
2025-02-09 21:03:24 +0100

    1-Installation de Laravel Breeze pour gérer l’inscription, la connexion et la réinitialisation des mots de passe.2-création des utilisateurs Via Tinker pour Vérifier que l’authentification fonctionne bien.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+
+
+
+
+
+Route::get('/', function () {
+    return view('auth.login'); // Remplace si nécessaire
+});
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +30,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+//Route::get('/', function () {
     return view('welcome');
+//});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
